16ecfa6cac73b014a3934b3d2bc2bcf34b62d769
pim-chan <tamba.chloe@outlook.fr>
2023-12-19 18:17:26 +0100

    modifications détails

--- index.js
+++ index.js
@@ -25,31 +25,28 @@ projectCards.forEach((projectCard, index) => {
 
 
 // Animation clic ancre 
-document.addEventListener('DOMContentLoaded', function () {
-    // Récupérer toutes les ancres dans le document
-    const anchors = document.querySelectorAll('a[href^="#"]');
-  
-    // Ajouter un gestionnaire d'événements à chaque ancre
-    anchors.forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-  
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-  
-        if (targetElement) {
-          // Calculer la position de défilement cible
-          const offsetTop = targetElement.offsetTop;
-  
-          // Faire défiler en douceur vers la cible
-          window.scrollTo({
-            top: offsetTop,
-            behavior: 'smooth'
-          });
-        }
-      });
+document.addEventListener('DOMContentLoaded', () => {
+  const anchors = document.querySelectorAll('a[href^="#"]');
+
+  anchors.forEach(anchor => {
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const targetId = anchor.getAttribute('href').substring(1); 
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        const offsetTop = targetElement.offsetTop;
+
+        window.scrollTo({
+          top: offsetTop,
+          behavior: 'smooth'
+        });
+      }
     });
   });
+});
+
   
 
 
